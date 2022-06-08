2173abea48f750babeba819cec2a942d69b60446
Boscotron <boscotron@users.noreply.github.com>
2022-06-08 01:12:13 -0600

    Create fake_db.ex

---
+++ lib/kajooly_template_tailwind_web/live/layouts_live/fake_db.ex
@@ -0,0 +1,58 @@
+defmodule KajoolyTemplateTailwindWeb.LayoutsLive.FakeDb do
+
+  def list() do
+    [
+      %{
+        title: "CAFÉ GILA CÁPSULAS 8X10",
+        subtitle: "CG-PS-10052",
+        created_by: "SARA BAUTISTA",
+        created: "2022-06-07 22:45 hrs.",
+        status: "REDD",
+        state: "ABIERTO",
+        update: "2022-06-07 22:45 hrs.",
+        to: "#"
+      },
+      %{
+        title: "CAFÉ GILA CÁPSULAS 8X10",
+        subtitle: "CG-PS-10052",
+        created_by: "SARA BAUTISTA",
+        created: "2022-06-07 22:45 hrs.",
+        status: "REDD",
+        state: "ABIERTO",
+        update: "2022-06-07 22:45 hrs.",
+        to: "#"
+      },
+      %{
+        title: "CAFÉ GILA CÁPSULAS 8X10",
+        subtitle: "CG-PS-10052",
+        created_by: "SARA BAUTISTA",
+        created: "2022-06-07 22:45 hrs.",
+        status: "REDD",
+        state: "ABIERTO",
+        update: "2022-06-07 22:45 hrs.",
+        to: "#"
+      },
+      %{
+        title: "CAFÉ GILA CÁPSULAS 8X10",
+        subtitle: "CG-PS-10052",
+        created_by: "SARA BAUTISTA",
+        created: "2022-06-07 22:45 hrs.",
+        status: "REDD",
+        state: "ABIERTO",
+        update: "2022-06-07 22:45 hrs.",
+        to: "#"
+      },
+      %{
+        title: "CAFÉ GILA CÁPSULAS 8X10",
+        subtitle: "CG-PS-10052",
+        created_by: "SARA BAUTISTA",
+        created: "2022-06-07 22:45 hrs.",
+        status: "REDD",
+        state: "ABIERTO",
+        update: "2022-06-07 22:45 hrs.",
+        to: "#"
+      }
+    ]
+  end
+
+end
