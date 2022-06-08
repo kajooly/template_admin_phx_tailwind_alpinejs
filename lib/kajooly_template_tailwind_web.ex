ff363337a710821999b2d28624e5315e005635e1
Boscotron <boscotron@users.noreply.github.com>
2022-06-07 19:53:23 -0600

    Update kajooly_template_tailwind_web.ex

--- lib/kajooly_template_tailwind_web.ex
+++ lib/kajooly_template_tailwind_web.ex
@@ -92,6 +92,7 @@ defmodule KajoolyTemplateTailwindWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import KajoolyTemplateTailwindWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
