8a62fd2c2cd823df06b03acbd49481a35528508b
Boscotron <boscotron@users.noreply.github.com>
2022-06-10 13:20:31 -0600

    static assets template

--- config/config.exs
+++ config/config.exs
@@ -31,24 +31,24 @@ config :esbuild,
   version: "0.14.0",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets/template --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+config :dart_sass,
+  version: "1.49.11",
+  default: [
+    args: ~w(css/app.scss ../priv/static/assets/template/style.tailwind.css),
+    cd: Path.expand("../assets", __DIR__)
   ]
 config :tailwind,
   version: "3.0.24",
   default: [
     args: ~w(
       --config=tailwind.config.js
-      --input=../priv/static/assets/app.tailwind.css
-      --output=../priv/static/assets/app.css
+      --input=../priv/static/assets/template/style.tailwind.css
+      --output=../priv/static/assets/template/style.css
     ),
-    cd: Path.expand("../assets", __DIR__)
-  ]
-config :dart_sass,
-  version: "1.49.11",
-  default: [
-    args: ~w(css/app.scss ../priv/static/assets/app.tailwind.css),
     cd: Path.expand("../assets", __DIR__)
   ]
 # Configures Elixir's Logger
