d364b0e0b384102222359040d976834b15a55fc3
Boscotron <boscotron@users.noreply.github.com>
2022-06-10 10:41:19 -0600

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -22,7 +22,7 @@ config :esbuild,
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+    env: %{"NODE_PATH" => Path.expand("../../../deps", __DIR__)}
   ]
 config :tailwind,
   version: "3.0.24",
