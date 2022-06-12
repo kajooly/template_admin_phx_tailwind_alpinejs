b3b371e0aaf3486b1628e9beb7073760a7e7b58d
Boscotron <boscotron@users.noreply.github.com>
2022-06-11 19:00:42 -0600

    Update table.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/table.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/table.ex
@@ -19,7 +19,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Table do
   """
   def table(assigns) do
     ~H"""
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg z-0">
+    <div class={"relative overflow-x-auto shadow-md sm:rounded-lg z-0 #{assigns[:class]}"}>
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
