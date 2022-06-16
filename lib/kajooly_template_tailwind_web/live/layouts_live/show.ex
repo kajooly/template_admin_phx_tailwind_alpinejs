3671611b1b9c88b89037973953951faacc385a22
Boscotron <boscotron@users.noreply.github.com>
2022-06-15 18:21:17 -0600

    Vista detalles brief

---
+++ lib/kajooly_template_tailwind_web/live/layouts_live/show.ex
@@ -0,0 +1,33 @@
+defmodule KajoolyTemplateTailwindWeb.LayoutsLive.Show do
+  use KajoolyTemplateTailwindWeb, :live_view
+
+  import  KajoolyTemplateTailwindWeb.GenericLive.Breadcrum
+  import KajoolyTemplateTailwindWeb.GenericLive.Table
+  import KajoolyTemplateTailwindWeb.GenericLive.Button
+  import KajoolyTemplateTailwindWeb.GenericLive.ButtonGroup
+  import KajoolyTemplateTailwindWeb.GenericLive.Dropdown
+  import KajoolyTemplateTailwindWeb.GenericLive.Badges
+  import KajoolyTemplateTailwindWeb.GenericLive.Search
+
+  import KajoolyTemplateTailwindWeb.LayoutsLive.FakeDb
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, list: list())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Show")
+    #|> put_flash(:info, "Alerta de información ")
+    #|> put_flash(:error, "Alerta de Error 404")
+    #|> put_flash(:warning, "Alerta de Aviso ")
+    #|> put_flash(:success, "Alerta de Éxito ")
+  end
+
+end
