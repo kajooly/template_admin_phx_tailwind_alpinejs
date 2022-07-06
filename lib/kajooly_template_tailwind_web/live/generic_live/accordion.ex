659d37f0b7d8538478d8b70da349acf0df7f5669
Boscotron <boscotron@users.noreply.github.com>
2022-07-06 02:14:56 -0600

    accordion

--- lib/kajooly_template_tailwind_web/live/generic_live/accordion.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/accordion.ex
@@ -80,7 +80,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Accordion do
               <% end %>
               </ul>
             <% else %>
-              <%= live_patch to: assigns[:to], class: "  " do %>
+              <%= live_patch to: assigns[:to], class: " flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 " do %>
                 <%= item[:title] || "subtítulo" %>
               <% end %>
             <% end %>
