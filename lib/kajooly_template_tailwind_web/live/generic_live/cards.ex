9c09ce22a31ad3bbd94599f8d8d594cb1ad54daa
Boscotron <boscotron@users.noreply.github.com>
2022-07-22 01:50:10 -0600

    Update cards.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/cards.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/cards.ex
@@ -64,4 +64,110 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Cards do
     """
   end
 
+  @doc """
+
+        <.list_card
+            title="Asignaciones"
+            to={Routes.layouts_issues_new_path(@socket, :assignments)}
+        >
+          <:list title="Título 1" sub_title="Subtitle asdasdasd......"
+            button_close="#"  />
+          <:list
+            title="Título 2"
+            sub_title="Subtitle asdasdasd......"
+            button_close="#"
+           />
+          <:list
+            title="Título 2"
+            sub_title="Subtitle asdasdasd......"
+            button_close="#"
+            to="#asd"
+           />
+          <:list title="Título 3" sub_title="Subtitle asdasdasd......">
+          adasad
+          </:list>
+        </.list_card>
+
+
+
+        <.list_card
+            title="Etiquetas"
+            list={[
+              %{
+                title: "titulo 1",
+                sub_title: "Subtitle asdasdasd......",
+                button_close: ""
+              }
+            ]}
+        >
+        </.list_card>
+
+
+  """
+  def list_card(assigns) do
+    ~H"""
+      <div class="w-full static ">
+        <div class="flex  mb-2">
+          <div class="flex-1">
+            <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-gray-600 text-left pl-4 mt-1"> <%= assigns[:title] %> </h6>
+          </div>
+          <div class="flex-0 pr-2">
+
+            <%= live_patch  to: assigns[:to], class: "sm:inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5", type: "button" do %>
+            <.icons_plus />
+
+              <% end %>
+
+          </div>
+        </div>
+        <div class="flex mb-3">
+          <div class="flex-1">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg max-w-[95%] mx-auto dark:border-2  dark:border-gray-600">
+              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+
+                <tbody>
+                  <%= for item <- assigns[:list] do %>
+                    <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <td scope="row" colspan="2" class="flex px-6 pt-1 pb-1 text-left font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                        <div class="flex-1 pt-1">
+                        <%= if item[:to] do  %>
+                          <%= live_patch  to: item[:to] do %>
+                            <%= item[:title] %>  <br/>
+                            <small class="text-gray-600 dark:text-gray-400"><em><%= item[:sub_title] %> </em></small>
+                          <% end %>
+                        <% else %>
+                          <%= item[:title] %>  <br/>
+                          <small class="text-gray-600 dark:text-gray-400"><em><%= item[:sub_title] %> </em></small>
+                        <% end %>
+
+                        </div>
+
+
+                        <div class="flex-0 pt-2">
+
+                        <%= if item[:button_close] do  %>
+                          <%= live_patch "X", to: item[:button_close], class: "text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1 text-center mr-2 mb-2 mt-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900", ":class": "isCompactMode ? 'py-2':'py-6" %>
+                        <% else %>
+                          <%= render_slot(item) %>
+                        <% end %>
+
+
+
+                        </div>
+
+                      </td>
+                    </tr>
+                  <% end %>
+
+
+                  </tbody>
+              </table>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    """
+  end
+
 end
