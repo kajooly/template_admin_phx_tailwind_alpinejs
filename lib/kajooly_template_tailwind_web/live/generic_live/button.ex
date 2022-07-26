a69f050a3ab869f28e436b4b7e5e9829bf7578f5
Boscotron <boscotron@users.noreply.github.com>
2022-07-25 20:42:45 -0600

    Update button.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/button.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/button.ex
@@ -35,7 +35,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Button do
   def button(assigns) do
     ~H"""
     <span x-data="{ isCompactModeButton: $persist(false).as('isCompactMode') }" class={"#{assigns[:class_span]} focus:z-10 #{ if assigns[:active] != nil do " z-10" end  } " }>
-    <%= live_patch to: assigns[:to], class: "px-4 text-center font-medium focus:ring-2 focus:z-10 #{assigns[:color_class] || "text-gray-900 bg-white   focus:ring-gray-700 focus:text-gray-700   dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-500 dark:focus:text-white dark:text-gray-300 hover:text-gray-700 "} #{assigns[:border] || "dark:border-gray-600 border-gray-200 hover:bg-gray-100 "} #{assigns[:class]} #{
+    <%= live_patch to: assigns[:to], class: "px-4 text-center font-medium focus:ring-2 focus:z-10 #{assigns[:color_class] || "text-gray-900 bg-white dark:bg-gray-700   focus:ring-gray-700 focus:text-gray-700   dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-500 dark:focus:text-white dark:text-gray-300 hover:text-gray-700 "} #{assigns[:border] || "dark:border-gray-600 border-gray-200 hover:bg-gray-100 "} #{assigns[:class]} #{
       case assigns[:borderradius] do
       "left" ->  "rounded-l-lg border border-r-0"
       "center" -> "border-t border-b"
