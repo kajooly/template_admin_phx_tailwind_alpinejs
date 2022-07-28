7a1804eaf4a072bc75c6672e3a8b0788f4a60e0c
Boscotron <boscotron@users.noreply.github.com>
2022-07-27 18:46:24 -0600

    Update Table

--- lib/kajooly_template_tailwind_web/live/generic_live/table.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/table.ex
@@ -20,11 +20,11 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Table do
   def table(assigns) do
     ~H"""
     <div class={"relative shadow-md sm:rounded-lg z-0 #{assigns[:class]}"} x-data="{ isCompactModeButton: $persist(false).as('isCompactMode') }">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-t-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-lg">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 rounded-t-lg ">
           <tr class="rounded-t-lg  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <%= for head <- @head do %>
-              <th scope="col"  colspan={head[:colspan]||"1"} class={"px-4 py-0 align-middle #{head[:class]}"}>
+              <th scope="col"  colspan={head[:colspan]||"1"} class={" #{head[:class]||"px-4 py-3 align-middle"}"}>
                 <%= assigns[:label] || render_slot(head) %>
               </th>
             <% end %>
@@ -33,9 +33,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Table do
         <tbody>
           <%= for row <- @rows do %>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700"
-                :class="isCompactModeButton ? ' md:leading-3':' md:leading-5'"
-
-                >
+                :class="isCompactModeButton ? ' md:leading-3':' md:leading-5'" >
               <%= for col <- @col do %>
                 <%= if col[:th]!=nil do %>
                   <th scope="row"
