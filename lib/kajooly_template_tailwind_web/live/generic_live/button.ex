2e05a3dec5f7b781a51cda343104faf4bca09155
Boscotron <boscotron@users.noreply.github.com>
2022-07-30 04:02:24 -0600

    Update button.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/button.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/button.ex
@@ -2,6 +2,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Button do
   import Phoenix.LiveView
   import Phoenix.LiveView.Helpers
   import KajoolyTemplateTailwindWeb.GenericLive.Text
+  import KajoolyTemplateTailwindWeb.GenericLive.Badges
 
   @doc """
 
@@ -56,15 +57,16 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Button do
 
 
   def default_class_button(assigns) do
-    "px-4 text-center font-medium focus:ring-2 focus:z-10 #{assigns[:color_class] || "text-gray-900 bg-white dark:bg-gray-700   focus:ring-gray-700 focus:text-gray-700   dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-500 dark:focus:text-white dark:text-gray-300 hover:text-gray-700 "} #{assigns[:border] || "dark:border-gray-600 border-gray-200 hover:bg-gray-100 "} #{assigns[:class]} #{
+    "px-4 text-center font-medium focus:ring-2 focus:z-10 #{assigns[:color_class] || "text-gray-900 bg-white dark:bg-gray-700   focus:ring-gray-700 focus:text-gray-700   dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-500 dark:focus:text-white dark:text-gray-300 hover:text-gray-700 "}  #{assigns[:class]} #{
       case assigns[:borderradius] do
-      "left" ->  "rounded-l-lg border border-r-0"
+      "left" ->  "rounded-l-md border border-r-0"
       "center" -> "border-t border-b"
       "right" -> "rounded-r-md border border-l-0"
       "only" -> "rounded-lg border"
       "box" -> " border"
       _ -> ""
       end}
+      #{assigns[:border] || "dark:border-gray-600 border-gray-200 hover:bg-gray-100 "}
       #{ if assigns[:active] != nil do " border-l ring-1 ring-sky-400 bg-gray-100 dark:bg-gray-600" else " " end  }
       "
   end
@@ -98,8 +100,15 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Button do
 
   """
   def circle_button(assigns) do
+    id = assigns[:id] || gen_id_key()
     ~H"""
-    <span x-data="{ isCompactMode: $persist(false).as('isCompactMode') }" >
+
+    <span x-data="{ isCompactMode: $persist(false).as('isCompactMode'), tooltip : false }" @mouseover.away="{tooltip = false}"  >
+    <span class="absolute "  x-show="tooltip" x-collapse>
+      <.badge color="danger">
+        <%= assigns[:title] %>
+      </.badge>
+    </span>
     <%= live_patch to: assigns[:to], class: " flex items-center justify-center  font-medium rounded-full  focus:z-10   #{assigns[:border] || " border-2 border-white dark:border-gray-800 "} #{assigns[:color_class] || " text-gray-600 dark:text-white bg-gray-300 dark:bg-gray-700 hover:bg-gray-600 dark:hover:bg-gray-600 hover:text-gray-100 "} #{assigns[:class]} #{
       case assigns[:borderradius] do
       "only" -> " "
@@ -107,7 +116,9 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Button do
       end}",
       ":class": "isCompactMode ? 'w-8 h-8 text-xs':'w-10 h-10 text-sm'",
       style: assigns[:style] || "__no_style: false;",
-      type: "button"
+      type: "button",
+      "alt": assigns[:title] || "",
+      id: id
       do %>
       <%= if assigns[:two_charter] do %>
         <%= text_to_two_char(assigns[:title]) %>
