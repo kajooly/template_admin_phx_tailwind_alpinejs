38c9dc9c50bca64219a1c2d66fc7903df2df85b1
Boscotron <boscotron@users.noreply.github.com>
2022-08-25 10:50:52 -0600

    Update breadcrum.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/breadcrum.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/breadcrum.ex
@@ -49,48 +49,54 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Breadcrum do
             <small><%= assigns[:subtitle]  %></small>
           </h1>
         <% end %>
-        <ol class="inline-flex items-center space-x-1 md:space-x-1 ml-auto mb-auto font-medium"
-            >
-          <%= if @page != nil do %>
-            <%= for item <- @page do %>
-              <%= if item[:to] != nil do %>
-                <li>
-                  <div class="flex items-center">
-                  <%= live_patch to: item[:to], class: " flex text-gray-700 hover:text-gray-900  dark:text-gray-400 dark:hover:text-white" do %>
-                    <%=  if item[:icon] == "home" do %>
-                      <.icons_home class="flex-0 w-4 h-4"/>
-                    <% end %>
-                    <%=  if item[:icon] == "chevron" do %>
-                      <.icons_chevron_right class="flex-0 w-6 h-6 text-gray-400"/>
-                    <% end %>
-                      <span class="flex-1 mt-1"
-                      :class=" isCompactMode ? 'text-xs':'text-xs' "
-                      ><%= item[:title] || render_slot(item) %></span>
-                    <% end %>
-                  </div>
-                </li>
-              <% else %>
-                <li aria-current="page">
-                  <div class="flex items-center">
-                    <%=  if item[:icon] == "home" do %>
-                      <.icons_home class="mr-2 w-4 h-4"/>
-                    <% end %>
-                    <%=  if item[:icon] == "chevron" do %>
-                      <.icons_chevron_right class="w-6 h-6 text-gray-400"/>
-                    <% end %>
-                    <span
-                      class="ml-1  text-gray-400 dark:text-gray-500 md:ml-2 dark:text-gray-400"
-                      :class=" isCompactMode ? 'text-xs':'text-sm ' "
-                      > <%= item[:title] || render_slot(item) %></span>
-                  </div>
-                </li>
-              <% end %>
-            <% end %>
-          <% end %>
-        </ol>
+        <.breadcrum_list pages={@page} />
       </nav>
     </span>
     """
+  end
+
+  def breadcrum_list(assigns) do
+      ~H"""
+      <ol class="inline-flex items-center space-x-1 md:space-x-1 ml-auto mb-auto font-medium"
+      >
+        <%= if assigns[:pages] != nil do %>
+          <%= for item <- assigns[:pages] do %>
+            <%= if item[:to] != nil do %>
+              <li>
+                <div class="flex items-center">
+                <%= live_patch to: item[:to], class: " flex text-gray-700 hover:text-gray-900  dark:text-gray-400 dark:hover:text-white" do %>
+                  <%=  if item[:icon] == "home" do %>
+                    <.icons_home class="flex-0 w-4 h-4"/>
+                  <% end %>
+                  <%=  if item[:icon] == "chevron" do %>
+                    <.icons_chevron_right class="flex-0 w-6 h-6 text-gray-400"/>
+                  <% end %>
+                    <span class="flex-1 mt-1"
+                    :class=" isCompactMode ? 'text-xs':'text-xs' "
+                    ><%= item[:title] || render_slot(item) %></span>
+                  <% end %>
+                </div>
+              </li>
+            <% else %>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <%=  if item[:icon] == "home" do %>
+                    <.icons_home class="mr-2 w-4 h-4"/>
+                  <% end %>
+                  <%=  if item[:icon] == "chevron" do %>
+                    <.icons_chevron_right class="w-6 h-6 text-gray-400"/>
+                  <% end %>
+                  <span
+                    class="ml-1  text-gray-400 dark:text-gray-500 md:ml-2 dark:text-gray-400"
+                    :class=" isCompactMode ? 'text-xs':'text-sm ' "
+                    > <%= item[:title] || render_slot(item) %></span>
+                </div>
+              </li>
+            <% end %>
+          <% end %>
+        <% end %>
+      </ol>
+      """
   end
 
 end
