3dc2c4edcdf406c7a65e5dc17f29ea63791fd5ec
Boscotron <boscotron@users.noreply.github.com>
2022-09-04 16:36:15 -0600

    Update badges.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/badges.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/badges.ex
@@ -10,6 +10,15 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Badges do
 
     import KajoolyTemplateTailwindWeb.GenericLive.Badges
 
+
+    Type:
+      - span - default
+      - div
+      - link
+
+    To:
+      to - live_patch(assigns[:to])
+
     Colors:
     - secondary
     - danger
@@ -22,55 +31,70 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Badges do
 
   ## Examples
 
+      <.badge title="Badges title"/>
+
+      ----
+
       <.badge {%{ color: "danger", title: "Badges title"}} />
+
       ----
-      <.badge color="danger">
-          Badges title
+
+      <.badge color="danger" type="div">
+        Badges title
       </.badge>
+
       ----
+
       <.badge
         color="custom"
         custom="bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
+        to="#"
       >
         Badges title
       </.badge>
+
   """
   def badge(assigns) do
-    ~H"""
-      <%= if assigns[:type] == "div" do %>
+    op = if assigns[:to] != nil do "link" else assigns[:type] end
+    case op do
+      "link" ->
+        ~H"""
+          <%= live_patch to: assigns[:to],
+            style: "#{assigns[:style]}",
+            title: "#{assigns[:alt_title] || assigns[:title]}",
+            class: badge_class(assigns),
+            type: "button" do
+            %><div class="flex"><%= assigns[:title] || render_slot(@inner_block) %></div><% end %>
+        """
+      "div" ->
+        ~H"""
+          <div
+            style={"#{assigns[:style]}"}
+            title={"#{assigns[:alt_title] || assigns[:title]}"}
+            class={badge_class(assigns)}><div class="flex"><%= assigns[:title] || render_slot(@inner_block) %></div></div>
+        """
+      _ ->
+        ~H"""
+          <span
+            style={"#{assigns[:style]}"}
+            title={"#{assigns[:alt_title] || assigns[:title]}"}
+            class={badge_class(assigns)}><%= assigns[:title] || render_slot(@inner_block) %></span>
+        """
+    end
 
-      <div
-        style={"#{assigns[:style]}"}
-        title={"#{assigns[:alt_title] || assigns[:title]}"}
-        class={"text-xs font-semibold mr-2 px-2.5 py-0.5 rounded #{case assigns[:color] do
-        "secondary" -> "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300"
-        "danger" -> "bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900"
-        "success" -> "bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900"
-        "warning" -> "bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900"
-        "indigo" -> "bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900"
-        "purple" -> "bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900"
-        "pink" -> "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
-        "custom" -> assigns[:custom] || "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
-        _ -> "bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800"
-      end}"}><%= assigns[:title] || render_slot(@inner_block) %></div>
-
-      <% else %>
-      <span
-        style={"#{assigns[:style]}"}
-        title={"#{assigns[:alt_title] || assigns[:title]}"}
-        class={"text-xs font-semibold mr-2 px-2.5 py-0.5 rounded #{case assigns[:color] do
-        "secondary" -> "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300"
-        "danger" -> "bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900"
-        "success" -> "bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900"
-        "warning" -> "bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900"
-        "indigo" -> "bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900"
-        "purple" -> "bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900"
-        "pink" -> "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
-        "custom" -> assigns[:custom] || "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
-        _ -> "bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800"
-      end}"}><%= assigns[:title] || render_slot(@inner_block) %></span>
-      <% end %>
-    """
   end
 
+  def badge_class(assigns) do
+    "text-xs font-semibold mr-2 px-2.5 py-0.5 rounded #{case assigns[:color] do
+      "secondary" -> "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+      "danger" -> "bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900"
+      "success" -> "bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900"
+      "warning" -> "bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900"
+      "indigo" -> "bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900"
+      "purple" -> "bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900"
+      "pink" -> "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
+      "custom" -> assigns[:custom] || "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
+      _ -> "bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800"
+    end}"
+  end
 end
