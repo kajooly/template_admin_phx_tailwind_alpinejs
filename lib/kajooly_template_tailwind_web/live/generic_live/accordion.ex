9eb28c1d62acf3c4c3122def9904bf564446c271
Boscotron <boscotron@users.noreply.github.com>
2022-10-22 05:44:22 -0500

    more options in title accordion

--- lib/kajooly_template_tailwind_web/live/generic_live/accordion.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/accordion.ex
@@ -182,8 +182,15 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Accordion do
         :class="expanded ? isCompactMode ? 'px-5 py-2 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white': 'p-5 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white'  : isCompactMode ? 'px-5 py-2': 'p-5'"
         :class=" isCompactMode ? 'px-5 py-2': 'p-5'"
         @click="expanded = ! expanded" >
-          <span><%= assigns[:title] %></span>
-          <svg data-accordion-icon="" class="w-6 h-6  shrink-0" :class=" expanded ? 'rotate-180' : '' "
+          <span class="flex-0"><%= assigns[:title] %></span>
+          <%= if assigns[:head_title] != nil do %>
+            <%= for item  <- assigns[:head_title]  do %>
+              <span class={"flex-1 #{item[:class]}"}>
+                <%= item[:head_title] || render_slot(item) %>
+              </span>
+            <% end %>
+          <% end %>
+          <svg data-accordion-icon="" class="w-6 h-6 flex-0 shrink-0" :class=" expanded ? 'rotate-180' : '' "
           fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
         <div x-show="expanded" x-collapse
