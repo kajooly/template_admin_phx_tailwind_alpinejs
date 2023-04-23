4d2f4fdbe28a5a1560cb188a95584ebb83969aa2
Boscotron <boscotron@users.noreply.github.com>
2023-04-22 19:28:27 -0600

    Update badges.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/badges.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/badges.ex
@@ -86,15 +86,15 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Badges do
 
   def badge_class(assigns) do
     "#{assigns[:text]||"text-xs font-semibold"} mr-2 px-2.5 py-0.5 rounded #{case assigns[:color] do
-      "secondary" -> "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300"
-      "danger" -> "bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900"
-      "success" -> "bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900"
-      "warning" -> "bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900"
-      "indigo" -> "bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900"
-      "purple" -> "bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900"
-      "pink" -> "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
-      "custom" -> assigns[:custom] || "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900"
-      _ -> "bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800"
+      "secondary" -> "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300 #{assigns[:class]}"
+      "danger" -> "bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900 #{assigns[:class]}"
+      "success" -> "bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900 #{assigns[:class]}"
+      "warning" -> "bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900 #{assigns[:class]}"
+      "indigo" -> "bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900 #{assigns[:class]}"
+      "purple" -> "bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900 #{assigns[:class]}"
+      "pink" -> "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900 #{assigns[:class]}"
+      "custom" -> assigns[:custom] || "bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900 #{assigns[:class]}"
+      _ -> "bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 #{assigns[:class]}"
     end}"
   end
 end
