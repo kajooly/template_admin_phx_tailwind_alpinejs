6f702391733e767fabc8c83f574ec178fc5acaac
Boscotron <boscotron@users.noreply.github.com>
2023-09-05 16:15:12 -0600

    scrollAt

--- assets/js/bsk_hooks.js
+++ assets/js/bsk_hooks.js
@@ -233,6 +233,14 @@ BSKHooks.shortUrlToLatlon = {
     }
 }
 
+
+let scrollAt = () => {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+    let clientHeight = document.documentElement.clientHeight
+
+    return scrollTop / (scrollHeight - clientHeight) * 100
+}
 BSKHooks.InfiniteScroll = {
     page() { return this.el.dataset.page },
     mounted() {
