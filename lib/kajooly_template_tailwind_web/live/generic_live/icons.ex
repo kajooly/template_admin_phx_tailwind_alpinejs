e5f4572db10fc24137c976fdad65446181ced14a
Boscotron <boscotron@users.noreply.github.com>
2023-11-17 03:29:07 -0600

    Update icons.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/icons.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/icons.ex
@@ -854,7 +854,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Icons do
   end
   def icons_shopping_bag(assigns) do
     ~H"""
-      <svg xmlns="http://www.w3.org/2000/svg"  id={assigns[:id]||"icons_shopping_bag#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"none"}  class={" #{assigns[:class]}"} stroke-width="1.5" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg"  id={assigns[:id]||"icons_shopping_bag#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"none"}  class={" #{assigns[:class]}"} stroke-width="1.5" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
       </svg>
     """
@@ -862,7 +862,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Icons do
   @spec icons_shopping_cart(any()) :: Phoenix.LiveView.Rendered.t()
   def icons_shopping_cart(assigns) do
     ~H"""
-      <svg xmlns="http://www.w3.org/2000/svg"  id={assigns[:id]||"icons_shopping_cart#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"none"}  class={" #{assigns[:class]}"} stroke-width="1.5" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg"  id={assigns[:id]||"icons_shopping_cart#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"none"}  class={" #{assigns[:class]}"} stroke-width="1.5" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
       </svg>
 
