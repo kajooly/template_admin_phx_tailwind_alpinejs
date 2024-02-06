b8b2cda898bac7c518ac61e38433b98f9507d5d3
Boscotron <boscotron@users.noreply.github.com>
2024-02-05 22:46:41 -0600

    Update icons.ex

--- lib/kajooly_template_tailwind_web/live/generic_live/icons.ex
+++ lib/kajooly_template_tailwind_web/live/generic_live/icons.ex
@@ -150,7 +150,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Icons do
 
   def icons_search(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" id={assigns[:id]||"icons_search#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"currentColor"} class={"bi bi-search #{assigns[:class]}"}>
+    <svg xmlns="http://www.w3.org/2000/svg" id={assigns[:id]||"icons_search#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"currentColor"} class={"bi bi-search #{assigns[:class]}"} viewBox="0 0 16 16">
       <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
     </svg>
     """
@@ -158,7 +158,7 @@ defmodule KajoolyTemplateTailwindWeb.GenericLive.Icons do
 
   def icons_plus(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" id={assigns[:id]||"icons_plus#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"currentColor"} class={"bi bi-plus #{assigns[:class]}"}>
+    <svg xmlns="http://www.w3.org/2000/svg" id={assigns[:id]||"icons_plus#{gen_id_key()}"} width={assigns[:width]||"16"} height={assigns[:height]||"16"} fill={assigns[:fill]||"currentColor"} class={"bi bi-plus #{assigns[:class]}"} viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
       <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
     </svg>
